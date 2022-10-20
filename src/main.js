a97bb62135546c39a656f9acb25a6e9096347839
Gui Siebert <guilhermebsiebert@gmail.com>
2022-10-20 18:20:25 -0300

    após aula 01

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img ")
+console.log(ccLogo.src)
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#2D65F2", "#436D99"],
+    mastercard: ["#C69347", "#DF6F29"],
+    default: ["black", "grey"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
