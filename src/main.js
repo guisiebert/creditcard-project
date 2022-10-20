52a0ca23e2b338a6196d5660524088ecb714495a
Gui Siebert <guilhermebsiebert@gmail.com>
2022-10-20 19:13:07 -0300

    Pausa em 35:30 da aula 02

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -19,3 +20,27 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+// Security code MASK!
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = { mask: "0000" }
+IMask(securityCode, securityCodePattern)
+
+// Expiration Date
+const expDate = document.querySelector("#expiration-date")
+const expDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+}
+const expDateMasked = IMask(expDate, expDatePattern)
